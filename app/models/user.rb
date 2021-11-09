1d6dfaf4674234b9321c79157d1dc2f979f3c1ef
kalu6334 <martin.lucas76334@gmail.com>
2021-11-09 19:08:20 +0100

    First commit and pushing to Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  after_create :welcome_send
+  after_create :welcome_send 
    
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
     has_many :attendances
     has_many :events, through: :attendances
+
     def welcome_send
         UserMailer.welcome_email(self).deliver_now
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :attendances
   resources :events
   resources :users
-  root 'events#index'
+  root to: 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
